611a013286e8915adb41361940a856382832b206
ihaydinn <ihaydin7@gmail.com>
2021-12-10 18:46:46 +0300

    Add put mapping for user entity

--- src/main/java/com/ihaydin/budgetservice/service/UserService.java
+++ src/main/java/com/ihaydin/budgetservice/service/UserService.java
@@ -4,8 +4,12 @@ package com.ihaydin.budgetservice.service;
 import com.ihaydin.budgetservice.model.User;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface UserService {
     List<User> findAll();
+    Optional<User> findById(UUID uuid);
     User createUser(User user);
+    User updateUser(User user);
 }
