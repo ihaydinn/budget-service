561af14b812f9ed3f307c800047ce2e5ff7362ba
ihaydinn <ihaydin7@gmail.com>
2022-01-06 21:04:58 +0300

    Add get single bank service

--- src/main/java/com/ihaydin/budgetservice/service/BankService.java
+++ src/main/java/com/ihaydin/budgetservice/service/BankService.java
@@ -3,7 +3,10 @@ package com.ihaydin.budgetservice.service;
 import com.ihaydin.budgetservice.model.Bank;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BankService {
     List<Bank> findAll();
+    Optional<Bank> findById(Long id);
+
 }
