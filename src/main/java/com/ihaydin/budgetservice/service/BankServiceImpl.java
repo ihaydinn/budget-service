705a26ef16f570de33a4e7948775f925df466ba8
ihaydinn <ihaydin7@gmail.com>
2022-01-06 21:07:29 +0300

    Add get single bank service impl

--- src/main/java/com/ihaydin/budgetservice/service/BankServiceImpl.java
+++ src/main/java/com/ihaydin/budgetservice/service/BankServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BankServiceImpl implements BankService {
@@ -17,5 +18,10 @@ public class BankServiceImpl implements BankService {
     @Override
     public List<Bank> findAll() {
         return bankRepository.findAll();
+    }
+
+    @Override
+    public Optional<Bank> findById(Long id) {
+        return bankRepository.findById(id);
     }
 }
