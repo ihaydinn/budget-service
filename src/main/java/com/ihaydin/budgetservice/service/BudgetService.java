80ab8a65b6fb4e9bbb726b961df26f0ca892ad68
ihaydinn <ihaydin7@gmail.com>
2022-01-06 20:15:20 +0300

    Add budget single get request

--- src/main/java/com/ihaydin/budgetservice/service/BudgetService.java
+++ src/main/java/com/ihaydin/budgetservice/service/BudgetService.java
@@ -3,7 +3,9 @@ package com.ihaydin.budgetservice.service;
 import com.ihaydin.budgetservice.model.Budget;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BudgetService {
     List<Budget> findAll();
+    Optional<Budget> findById(Long id);
 }
