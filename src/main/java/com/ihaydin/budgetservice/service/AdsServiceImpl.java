b5859865d41caf220f621ef46ee73b46b698fb38
ihaydinn <ihaydin7@gmail.com>
2022-01-13 23:26:53 +0300

    Add ads service impl

---
+++ src/main/java/com/ihaydin/budgetservice/service/AdsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.ihaydin.budgetservice.service;
+
+import com.ihaydin.budgetservice.model.Ads;
+import com.ihaydin.budgetservice.repository.AdsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * @author ismailhakkiaydin
+ * @date 13, January, 2022
+ */
+
+@Service
+public class AdsServiceImpl implements AdsService {
+
+    @Autowired
+    AdsRepository adsRepository;
+
+    @Override
+    public Optional<Ads> findById(Long id) {
+        return adsRepository.findById(id);
+    }
+
+    @Override
+    public Ads updateAds(Ads ads) {
+        return adsRepository.save(ads);
+    }
+}
