8b8487550a3472b47de7d31474534141b725749e
ihaydinn <ihaydin7@gmail.com>
2022-01-20 11:51:41 +0300

    Add saving and budget dto

---
+++ src/main/java/com/ihaydin/budgetservice/dto/BudgetDto.java
@@ -0,0 +1,28 @@
+package com.ihaydin.budgetservice.dto;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author ismailhakkiaydin
+ * @date 19, January, 2022
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BudgetDto {
+
+    private Long id;
+
+    private String category;
+
+    private String targetValue;
+
+    private Long time;
+
+    private boolean isActive;
+
+}
---
+++ src/main/java/com/ihaydin/budgetservice/dto/SavingDto.java
@@ -0,0 +1,21 @@
+package com.ihaydin.budgetservice.dto;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author ismailhakkiaydin
+ * @date 19, January, 2022
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SavingDto {
+    private Long id;
+
+    @NotNull
+    private String amount;
+}
